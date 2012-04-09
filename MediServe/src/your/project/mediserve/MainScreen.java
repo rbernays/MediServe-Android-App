e9c3171368ad91f481683fd7dbb7113dfb8e0865
Juan Guzman <jcg_8319@yahoo.com>
2012-04-09 10:55:04 -0700

    Test push

---
+++ MediServe/src/your/project/mediserve/MainScreen.java
@@ -0,0 +1,22 @@
+package your.project.mediserve;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+public class MainScreen extends Activity {
+
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainscreen);
+		
+	}
+}
