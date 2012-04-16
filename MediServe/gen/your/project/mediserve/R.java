9554c6904645c6b6207685bca0eb730423f54fbb
Ryan Bernays <rbernays@asu.edu>
2012-04-16 13:27:56 -0700

    Latest commit, seeing if this one makes it.

--- MediServe/gen/your/project/mediserve/R.java
+++ MediServe/gen/your/project/mediserve/R.java
@@ -15,39 +15,54 @@ public final class R {
     }
     public static final class id {
         public static final int TextView01=0x7f050004;
-        public static final int button1=0x7f050009;
+        public static final int add_caregiver_button=0x7f050020;
+        public static final int add_education_button=0x7f05001f;
+        public static final int add_intervention_button=0x7f05001d;
+        public static final int add_patient_button=0x7f05000e;
+        public static final int add_problem_button=0x7f05001c;
+        public static final int button1=0x7f050019;
         public static final int cancel_button=0x7f050006;
-        public static final int changePass_button=0x7f05000c;
+        public static final int changePass_button=0x7f050017;
         public static final int confirmpassword=0x7f050003;
-        public static final int list=0x7f05000d;
-        public static final int login_button=0x7f05000b;
+        public static final int editText1=0x7f050010;
+        public static final int education=0x7f05001e;
+        public static final int find_patients=0x7f050009;
+        public static final int information=0x7f050021;
+        public static final int list=0x7f05000c;
+        public static final int login_button=0x7f050016;
+        public static final int my_patients=0x7f05000d;
         public static final int newpassword=0x7f050002;
         public static final int ok_button=0x7f050005;
         public static final int oldpassword=0x7f050001;
-        public static final int password=0x7f05000a;
+        public static final int password=0x7f050015;
+        public static final int patientname=0x7f05001a;
+        public static final int planofcare=0x7f05001b;
         public static final int result=0x7f050007;
-        public static final int tab1=0x7f050010;
-        public static final int tab2=0x7f050012;
-        public static final int tab3=0x7f050014;
-        public static final int tabHost=0x7f05000f;
-        public static final int textView1=0x7f050008;
-        public static final int txt1=0x7f050011;
-        public static final int txt2=0x7f050013;
-        public static final int txt3=0x7f050015;
+        public static final int searchButton=0x7f05000b;
+        public static final int searchText=0x7f05000a;
+        public static final int tabHost=0x7f050008;
+        public static final int tableRow1=0x7f050011;
+        public static final int tableRow2=0x7f050012;
+        public static final int tableRow3=0x7f050013;
+        public static final int tableRow4=0x7f050014;
+        public static final int textView1=0x7f05000f;
+        public static final int txt3=0x7f050022;
         public static final int username=0x7f050000;
-        public static final int viewer=0x7f05000e;
+        public static final int viewer=0x7f050018;
     }
     public static final class layout {
         public static final int buttonshape1=0x7f030000;
         public static final int changepass=0x7f030001;
         public static final int education=0x7f030002;
-        public static final int findpatients=0x7f030003;
-        public static final int information=0x7f030004;
-        public static final int main=0x7f030005;
-        public static final int mainhorizontal=0x7f030006;
-        public static final int mainscreen=0x7f030007;
-        public static final int patientinformation=0x7f030008;
-        public static final int planofcare=0x7f030009;
+        public static final int findmypatients=0x7f030003;
+        public static final int findpatients=0x7f030004;
+        public static final int information=0x7f030005;
+        public static final int main=0x7f030006;
+        public static final int mainhorizontal=0x7f030007;
+        public static final int mainscreen=0x7f030008;
+        public static final int mypatients=0x7f030009;
+        public static final int patientinformation=0x7f03000a;
+        public static final int planofcare=0x7f03000b;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- MediServe/src/your/project/mediserve/PatientInfo.java
+++ MediServe/src/your/project/mediserve/PatientInfo.java
@@ -16,16 +16,16 @@ public class PatientInfo extends Activity{
         tabHost.setup();
 
         TabSpec spec1=tabHost.newTabSpec("Tab 1");
-        spec1.setContent(R.id.tab1);
+        spec1.setContent(R.id.planofcare);
         spec1.setIndicator("Tab 1");
 
         TabSpec spec2=tabHost.newTabSpec("Tab 2");
         spec2.setIndicator("Tab 2");
-        spec2.setContent(R.id.tab2);
+        spec2.setContent(R.id.education);
 
         TabSpec spec3=tabHost.newTabSpec("Tab 3");
         spec3.setIndicator("Tab 3");
-        spec3.setContent(R.id.tab3);
+        spec3.setContent(R.id.information);
 
         tabHost.addTab(spec1);
         tabHost.addTab(spec2);
